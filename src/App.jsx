2f94d80f0d0dd192d4afae627aa56297df9461d7
Shrutigupta431 <sg9956855@gmail.com>
2024-06-08 14:56:20 +0530

    reduxTodo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+       <AddTodo />
+       <Todos />
+    </div>
+  )
+}
+
+export default App
